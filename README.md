94127ccad007a34697ef2a5e3faa81aab02349c2
Cong Nguyen <congnguyent@nashtechglobal.com>
2023-02-09 21:37:36 +0700

    Implement source game

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+### Credits
+* Leena Graphics
+    * https://opengameart.org/content/skater-girl-2d-game-character-demo
+* Platform:
+    * https://www.gameart2d.com/free-platformer-game-tileset.html
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,24 +11,34 @@ void main() {
 
 class Leena extends FlameGame {
   SpriteComponent leena = SpriteComponent();
-
+  late TiledComponent homeMap;
   double gravity = 1.8;
   Vector2 velocity = Vector2(0, 0);
 
   @override
   Future<void> onLoad() async {
-    // TODO: implement onLoad
-    await super.onLoad();
+
+    homeMap = await TiledComponent.load('map.tmx', Vector2.all(32));
+    add(homeMap);
+
+    double mapWidth = 32.0 * homeMap.tileMap.map.width;
+    double mapHeight = 32.0 * homeMap.tileMap.map.height;
+
+    camera.viewport = FixedResolutionViewport(Vector2(mapWidth, mapHeight));
+
+    //add player
     leena
       ..sprite = await loadSprite('girl_push_single_00.png')
       ..size = Vector2.all(100)
       ..position = Vector2(100, 30);
     add(leena);
+
+    super.onLoad();
   }
 
   @override
   void update(double dt) {
-    if (leena.y < size.y - leena.height) {
+    if (leena.y < homeMap.height - leena.height) {
       velocity.y += gravity;
       leena.position.y += velocity.y * dt;
     }
